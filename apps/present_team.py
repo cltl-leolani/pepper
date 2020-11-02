1a99ec559e34b8080830d9077b7f34a1575c068b
Thomas Baier <tbaier@math.bme.hu>
2020-11-02 10:51:38 +0100

    Re-add present team app

---
+++ apps/present_team.py
@@ -0,0 +1,387 @@
+from random import choice
+from time import time, sleep
+
+from pepper.app_container import ApplicationContainer, Application
+from pepper.framework.application.brain import BrainComponent
+from pepper.framework.application.context import ContextComponent
+from pepper.framework.application.display import DisplayComponent
+from pepper.framework.application.face_detection import FaceRecognitionComponent
+from pepper.framework.application.intention import AbstractIntention
+from pepper.framework.application.motion import MotionComponent
+from pepper.framework.application.object_detection import ObjectDetectionComponent
+from pepper.framework.application.speech_recognition import SpeechRecognitionComponent
+from pepper.framework.application.statistics import StatisticsComponent
+from pepper.framework.application.subtitles import SubtitlesComponent
+from pepper.framework.application.text_to_speech import TextToSpeechComponent
+from pepper.framework.sensor.api import UtteranceHypothesis
+from pepper.knowledge import sentences, animations
+from pepper.language.generation.reply import reply_to_question
+from pepper.responder import *
+
+SPEAKER_NAME_THIRD = "Dear guest"
+SPEAKER_NAME = "Dear guest"
+SPEAKER_FACE = "HUMAN"
+DEFAULT_SPEAKER = "Human"
+TOPIC_NAME = "De Vrije Wereld to discuss robots and humans"
+
+TOPIC_ROBOT_THOUGHT = "Humans and robots should work together in teams. Use each others strength and compensate for weaknesses."
+
+LOCATION_NAME = "The botanical winter garden"
+VU_NAME_PHONETIC = r"\\toi=lhp\\ fraiE universitai_t Amster_dam \\toi=orth\\"
+
+IMAGE_VU = "https://www.vu.nl/nl/Images/VUlogo_NL_Wit_HR_RGB_tcm289-201376.png"
+IMAGE_SELENE = "http://wordpress.let.vupr.nl/understandinglanguagebymachines/files/2019/06/7982_02_34_Selene_Orange_Unsharp_Robot_90kb.jpg"
+IMAGE_LENKA = "http://wordpress.let.vupr.nl/understandinglanguagebymachines/files/2019/06/8249_Lenka_Word_Object_Reference_106kb.jpg"
+IMAGE_BRAM = "http://makerobotstalk.nl/files/2018/12/41500612_1859783920753781_2612366973928996864_n.jpg"
+IMAGE_PIEK = "http://www.cltl.nl/files/2019/10/8025_Classroom_Piek.jpg"
+
+TOPIC_QUERY = "robot"
+TOPIC_QUESTION = "How do you think humans and robots will work together?"
+TOPIC_ANSWER = "Do you have a question for me?"
+MIN_ANSWER_LENGTH = 4
+
+RESPONDERS = [
+    BrainResponder(),
+    VisionResponder(), PreviousUtteranceResponder(), IdentityResponder(), LocationResponder(), TimeResponder(),
+    QnAResponder(),
+    GreetingResponder(), GoodbyeResponder(), ThanksResponder(), AffirmationResponder(), NegationResponder(),
+    UnknownResponder()
+]
+
+
+class PresentTeamIntention(ApplicationContainer,
+                     AbstractIntention, StatisticsComponent,
+                     SubtitlesComponent, ContextComponent,
+                     ObjectDetectionComponent, FaceRecognitionComponent,
+                     SpeechRecognitionComponent, TextToSpeechComponent,
+                     BrainComponent,
+                     MotionComponent, DisplayComponent):
+    SUBTITLES_URL = "https://bramkraai.github.io/subtitle?text={}"
+
+    def __init__(self):
+        super(PresentTeamIntention, self).__init__()
+
+    def start(self):
+        super(PresentTeamIntention, self).start()
+        self.show_on_display(IMAGE_VU)
+
+    def say(self, text, animation=None, block=True):
+        super(PresentTeamIntention, self).say(text, animation, block)
+        sleep(1.5)
+
+    def show_text(self, text):
+        self.show_on_display(self.SUBTITLES_URL.format(text))
+
+
+class WaitForStartCueIntention(PresentTeamIntention):
+    START_CUE_TEXT = [
+        "hello",
+        "hallo",
+        "hi",
+        "morning",
+        "evening",
+        "afternoon",
+        "hoi",
+        "who are you",
+        "a guest arrived",
+        "you may begin",
+        "you may start",
+        "you can begin",
+        "you can start"
+    ]
+
+    GREET_TIMEOUT = 15  # Only Greet people once every X seconds
+
+    def __init__(self):
+        """Greets New and Known People"""
+        self.name_time = {}  # Dictionary of <name, time> pairs, to keep track of who is greeted when
+
+        super(WaitForStartCueIntention, self).__init__()
+
+        # Initialize Response Picker
+        self.response_picker = ResponsePicker(self, RESPONDERS)
+
+    def start(self):
+        super(WaitForStartCueIntention, self).start()
+
+        # Start Chat with Default Speaker
+        self.context.start_chat(DEFAULT_SPEAKER)
+
+    def on_face_known(self, faces):
+        """
+        On Person Event.
+        Called every time a known face is detected.
+        """
+        for person in faces:
+            if self.is_greeting_appropriate(person.name):
+                self.say("Hello, {}!".format(person.name))
+
+    def on_face_new(self, faces):
+        """
+        On New Person Event.
+        Called every time an unknown face is detected.
+        """
+
+        if self.is_greeting_appropriate("new"):
+            self.say("I see a new person!, Hello stranger!")
+
+    def is_greeting_appropriate(self, name):
+        """Returns True if greeting is appropriate and updates Greeting Time"""
+
+        # Appropriateness arises when
+        #  1. person hasn't been seen before, or
+        #  2. enough time has passed since last sighting
+        if name not in self.name_time or (time() - self.name_time[name]) > self.GREET_TIMEOUT:
+            # Store last seen time (right now) in name_time dictionary
+            self.name_time[name] = time()
+
+            # Return "Appropriate"
+            return True
+
+        # Return "Not Appropriate"
+        return False
+
+    def on_face(self, faces):
+        # If Start Face Cue is observed by Leolani -> Start Main Intention
+        if self.is_greeting_appropriate("new"):
+            self.say("I see a new person!, Hello stranger!")
+
+        # # Before was like this
+        # if any([self.on_face_new([face]) for face in faces]):
+        #     self.say("Ah, I can see {}! Let me begin!".format(SPEAKER_NAME_THIRD))
+        #     self.change_intention(IntroductionIntention())
+
+        # Changed to this
+        if len(faces) == 0:
+            self.say("Ah, I can see {}! Let me begin!".format(SPEAKER_NAME_THIRD))
+            self.change_intention(IntroductionIntention())
+
+    def on_chat_turn(self, utterance):
+
+        # If Start Text Cue is observed by Leolani -> Respond Happy & Start Main Intention
+        transcript = utterance.transcript.lower()
+        if any([cue in transcript for cue in self.START_CUE_TEXT]):
+            self.say("Oh, {}!".format(choice(sentences.HAPPY)), animation=animations.HAPPY)
+            self.change_intention(IntroductionIntention())
+            return
+
+
+class IntroductionIntention(PresentTeamIntention):
+    def __init__(self):
+        super(IntroductionIntention, self).__init__()
+
+    def start(self):
+        # Start Speech
+        super(IntroductionIntention, self).start()
+
+        self.context.start_chat(SPEAKER_NAME)
+        self.speech()
+
+    def speech(self):
+
+        # 1.1 - Welcome
+        self.say("Hello {}. Welcome to the {}. ...".format(SPEAKER_NAME, LOCATION_NAME), animations.BOW)
+        self.say("We are thrilled to have you here!")
+
+        # 1.2 - Introduction
+        self.say(r"I am lio-lawhni... My name means \\vct=50\\ Voice of an Angel \\vct=100\\. in Hawaiian.",
+                 animations.I)
+        self.say("I am built by students from the VU that come from all over the world. ", animations.ONCE_UPON_A_TIME)
+        self.show_on_display(IMAGE_SELENE)
+        self.say("Selene, from Mexico, designed my brain and thoughts!", animations.TABLET)
+        self.show_on_display(IMAGE_LENKA)
+        self.say("Lenka, from Serbia, taught me to understand language", animations.TABLET)
+        self.show_on_display(IMAGE_BRAM)
+        self.say("Bram, from the Netherlands, programmed me to perceive the world around me.", animations.TABLET)
+        self.tablet.show(IMAGE_PIEK)
+        self.say(
+            "Peek, from the Netherlands, and I, from France and Japan, work on identity, reference and perspective in language!",
+            animations.TABLET)
+        self.show_on_display(IMAGE_VU)
+
+        sleep(2.5)
+
+        # 2.1 - Topic Introduction
+        self.say("I believe you are here for " + TOPIC_NAME, animations.THINK)
+        self.say("I am just a robot, but I have my little robot thoughts...", animations.SHY)
+        self.say(TOPIC_ROBOT_THOUGHT)
+
+        # 2.2 - Topic Knowledge
+        self.say("As a robot, I have learned a few things about " + TOPIC_QUERY + " myself!", animations.THINK)
+
+        # # 2.2.1 - Topic in the News
+        # self.say("I have been reading the news lately!")
+        # self.say(choice(sentences.TOPIC_NEWS))
+        # self.say("{}!".format(choice(sentences.HAPPY)), animations.HAPPY)
+
+        # 2.2.2 - Topic in Brain
+        self.say("I also have been talking about " + TOPIC_NAME + " with my friends!")
+        self.topic_in_brain()
+
+        self.say("I learn a lot from my friends!")
+
+        sleep(2.5)
+
+        # Move to Topic QnA
+        self.change_intention(TopicQuestionIntention())
+
+    def topic_in_brain(self):
+        self.answer_brain_query("what is " + TOPIC_QUERY + " about")
+        self.answer_brain_query("what is " + TOPIC_QUERY + " ")
+
+    def answer_brain_query(self, query):
+        try:
+            question = self.context.chat.add_utterance([UtteranceHypothesis(query, 1)], False)
+            question.analyze()
+
+            brain_response = self.brain.query_brain(question)
+            reply = reply_to_question(brain_response)
+            if reply: self.say(reply, block=False)
+        except Exception as e:
+            self.log.error(e)
+
+
+# 2.3 - Topic Question
+class TopicQuestionIntention(PresentTeamIntention):
+    def __init__(self):
+        super(TopicQuestionIntention, self).__init__()
+
+        # Initialize Response Picker
+        self.response_picker = ResponsePicker(self, RESPONDERS)
+
+        self._retried = False
+
+    def start(self):
+        super(TopicQuestionIntention, self).start()
+
+        # Start Chat with Speaker if not already running
+        if not self.context.chatting:
+            self.context.start_chat(SPEAKER_NAME)
+
+        # Ask Topic Question
+        self.say("Oh {}, I think I have a question for you!".format(SPEAKER_NAME), animations.EXPLAIN)
+        self.show_text(TOPIC_QUESTION)
+        self.say(TOPIC_QUESTION)
+
+    def on_chat_turn(self, utterance):
+        responder = self.response_picker.respond(utterance)
+
+        if self.context.chat.last_utterance.transcript.endswith("?"):
+            self.say("Oops, nevermind me asking these questions. I'm just a very curious robot!", animations.ASHAMED)
+
+        # If Pepper does not understand?
+        if isinstance(responder, UnknownResponder) and len(utterance.tokens) < MIN_ANSWER_LENGTH and not self._retried:
+            # -> Repeat Question
+            self._retried = True
+            self.say("But, {}".format(TOPIC_QUESTION))
+        else:  # If a decent response can be formed
+            # -> Thank Speaker and Move on to TopicAnswerIntention
+            self.say("Thank you for your answer!", animations.HAPPY)
+            self.show_on_display(IMAGE_VU)
+            self.change_intention(TopicAnswerIntention())
+
+
+# 2.4 - Topic Answer
+class TopicAnswerIntention(PresentTeamIntention):
+    def __init__(self):
+        super(TopicAnswerIntention, self).__init__()
+
+        # Initialize Response Picker
+        self.response_picker = ResponsePicker(self, RESPONDERS)
+
+        self._retried = False
+
+    def start(self):
+        super(TopicAnswerIntention, self).start()
+
+        # Start Chat with Speaker if not already running
+        if not self.context.chatting:
+            self.context.start_chat(SPEAKER_NAME)
+
+        self.show_text(TOPIC_ANSWER)
+        self.say(TOPIC_ANSWER)
+
+    def on_chat_turn(self, utterance):
+        responder = self.response_picker.respond(utterance)
+
+        if self.context.chat.last_utterance.transcript.endswith("?"):
+            self.say("Oops, nevermind me asking these questions. I'm just a very curious robot!", animations.ASHAMED)
+
+        # If Pepper does not understand?
+        if isinstance(responder, UnknownResponder) and len(utterance.tokens) < MIN_ANSWER_LENGTH and not self._retried:
+            # -> Repeat Question
+            self._retried = True
+            self.say("But, {}".format(TOPIC_ANSWER))
+        else:  # If a decent response can be formed
+            # -> Thank Speaker and Move on to OutroIntention
+            self.say("Thank you!", animations.HAPPY)
+            self.show_on_display(IMAGE_VU)
+            self.change_intention(OutroIntention())
+
+
+class OutroIntention(PresentTeamIntention):
+    def __init__(self):
+        super(OutroIntention, self).__init__()
+
+        # Initialize Response Picker
+        self.response_picker = ResponsePicker(self, RESPONDERS)
+
+    def start(self):
+        super(OutroIntention, self).start()
+
+        # Start Chat with Speaker if not already running
+        if not self.context.chatting:
+            self.context.start_chat(SPEAKER_NAME)
+
+        self.speech()
+
+    def speech(self):
+        # 5.1 - Wish all a fruitful discussion
+        self.say("I see that there are {0} people here... I wish all {0} of you a fruitful discussion!".format(
+            len([obj for obj in self.context.objects if obj.name == "person"])), animations.HELLO)
+
+        # 5.2 - Goodbye
+        self.say("It's a pity we could not talk more and get to know each other.",
+                 animations.FRIENDLY)
+        self.say("It was nice having talked to you, {}! ... ...".format(SPEAKER_NAME), animations.BOW)
+
+        self.say("If you have any questions, you can always ask me later!")
+
+        sleep(4)
+
+        self.say("You may make a selfie with me! I love pictures!", animations.HAPPY)
+
+        # Switch to Default Intention
+        self.change_intention(DefaultIntention())
+
+
+class DefaultIntention(PresentTeamIntention):
+    IGNORE_TIMEOUT = 60
+
+    def __init__(self):
+        super(DefaultIntention, self).__init__()
+
+        self._ignored_people = {}
+        self.response_picker = ResponsePicker(self, RESPONDERS)
+
+    def on_chat_enter(self, name):
+        self._ignored_people = {n: t for n, t in self._ignored_people.items() if time() - t < self.IGNORE_TIMEOUT}
+
+        if name not in self._ignored_people:
+            self.context.start_chat(name)
+            self.say("{}, {}".format(choice(sentences.GREETING), name))
+
+    def on_chat_exit(self):
+        self.say("{}, {}".format(choice(sentences.GOODBYE), self.context.chat.speaker))
+        self.context.stop_chat()
+
+    def on_chat_turn(self, utterance):
+        responder = self.response_picker.respond(utterance)
+
+        if isinstance(responder, GoodbyeResponder):
+            self._ignored_people[utterance.chat.speaker] = time()
+            self.context.stop_chat()
+
+
+if __name__ == '__main__':
+    Application(PresentTeamIntention()).run()
